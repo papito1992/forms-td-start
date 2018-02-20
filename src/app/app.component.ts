3e8e3261f3647aba07070dccda016d92763f5436
papito1992 <pm@adsd.com>
2018-02-20 20:54:14 +0200

    FORMS TD DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {Ng4TwitterTimelineService} from 'ng4-twitter-timeline/lib';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +9,33 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') singupForm: NgForm;
-
+  defaultQuestion = 'pet';
+  answer: '';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    sq: '',
+    answer: '',
+    gender: ''
+  };
+submited = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.singupForm.form.patchValue({userData: {
+      username: suggestedName
+      }});
   }
 //   onSubmit(form: NgForm) {
 // console.log(form);
 //   }
   onSubmit() {
-    console.log(this.singupForm);
+    this.submited = true;
+    this.user.username = this.singupForm.value.userData.username;
+    this.user.email = this.singupForm.value.userData.email;
+    this.user.sq = this.singupForm.value.secret;
+    this.user.answer = this.singupForm.value.questionAnswer;
+    this.user.gender = this.singupForm.value.gender;
+    this.singupForm.reset();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {Ng4TwitterTimelineModule} from 'ng4-twitter-timeline/lib';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng4TwitterTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
